53234dea20ce20c843b945b101261f893007f458
TimofeyKanatush <t.kanatush@logic-way.com>
2022-06-15 15:01:49 +0300

    Hello triangle

---
+++ Assets/Scripts/MeshGenerator.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class MeshGenerator : MonoBehaviour
+{
+    private MeshFilter _meshFilter;
+    private MeshRenderer _meshRenderer;
+
+    private Mesh _mesh;
+
+    private Vector3[] _vertices;
+    private int[] _triangles;
+
+    private void Awake()
+    {
+        _meshFilter = GetComponent<MeshFilter>();
+        _meshRenderer = GetComponent<MeshRenderer>();
+
+        _mesh = new Mesh();
+        _meshFilter.mesh = _mesh;
+        
+        CreateMesh();
+        UpdateMesh();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        if (_vertices == null)
+        {
+            return;
+        }
+
+        for (int i = 0; i < _vertices.Length; i++)
+        {
+            Gizmos.DrawLine(_vertices[i], _mesh.normals[i]);
+        }
+    }
+
+    private void CreateMesh()
+    {
+        _vertices = new[]
+        {
+            new Vector3(0,0,0),
+            new Vector3(0,0,1),
+            new Vector3(1,0,0)
+        };
+
+        _triangles = new[]
+        {
+            0, 1, 2
+        };
+    }
+
+    private void UpdateMesh()
+    {
+        _mesh.Clear();
+
+        _mesh.vertices = _vertices;
+        _mesh.triangles = _triangles;
+
+        _mesh.RecalculateNormals();
+    }
+}
