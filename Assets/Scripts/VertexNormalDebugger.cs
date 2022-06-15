b4f82a6c24b8e83e68751265096b1685643a1d8d
TimofeyKanatush <t.kanatush@logic-way.com>
2022-06-15 16:24:54 +0300

    Fix local normals

--- Assets/Scripts/VertexNormalDebugger.cs
+++ Assets/Scripts/VertexNormalDebugger.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 [RequireComponent(typeof(MeshFilter))]
@@ -5,6 +6,8 @@ public class VertexNormalDebugger : MonoBehaviour
 {
     [SerializeField] private Color _color = Color.red;
     [SerializeField, Min(0)] private float _lenght = 0.5f;
+    [SerializeField] private bool _drawWithSelected;
+    
 
     private MeshFilter _meshFilter;
 
@@ -15,18 +18,43 @@ public class VertexNormalDebugger : MonoBehaviour
 
     private void OnDrawGizmosSelected()
     {
-        Gizmos.color = _color;
+        if (!_drawWithSelected)
+        {
+            return;
+        }
 
         if (_meshFilter == null || _meshFilter.mesh == null)
         {
             return;
         }
 
+        DrawNormals();
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (_drawWithSelected)
+        {
+            return;
+        }
+
+        if (_meshFilter == null || _meshFilter.mesh == null)
+        {
+            return;
+        }
+
+        DrawNormals();
+    }
+
+    private void DrawNormals()
+    {
+        Gizmos.color = _color;
+
         Mesh mesh = _meshFilter.mesh;
         for (var i = 0; i < mesh.normals.Length; i++)
         {
-            Vector3 startPosition = mesh.vertices[i];
-            Vector3 endPosition = startPosition + mesh.normals[i] * _lenght;
+            Vector3 startPosition = transform.TransformPoint(mesh.vertices[i]);
+            Vector3 endPosition = startPosition + transform.TransformDirection(mesh.normals[i]) * _lenght;
             
             Gizmos.DrawLine(startPosition, endPosition);
         }
