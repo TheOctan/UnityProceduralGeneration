e210707c5d86d09cfef2b9db4f0ff552f08edc2b
TheOctan <kanatush.tim@gmail.com>
2022-07-08 14:47:33 +0300

    Update linear converter and add generic extensions

---
+++ Assets/Scripts/Extensions/GenericExtensions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OctanGames.Extensions
+{
+    public static class GenericExtensions
+    {
+        public static IList<T> Shuffle<T>(this IList<T> list)
+        {
+            return list.Shuffle(DateTime.Now.Second);
+        }
+
+        public static IList<T> Shuffle<T>(this IList<T> list, int seed)
+        {
+            var random = new Random(seed);
+            for (var i = 0; i < list.Count - 1; i++)
+            {
+                int randomIndex = random.Next(i, list.Count);
+
+                (list[randomIndex], list[i]) = (list[i], list[randomIndex]);
+            }
+
+            return list;
+        }
+
+        /// <summary>
+        /// Returns min and max values from map
+        /// </summary>
+        /// <param name="map"></param>
+        /// <returns></returns>
+        public static (float min, float max) MinMax(this float[,] map)
+        {
+            int width = map.GetLength(0);
+            int height = map.GetLength(1);
+
+            var max = float.MinValue;
+            var min = float.MaxValue;
+
+            for (var y = 0; y < height; y++)
+            {
+                for (var x = 0; x < width; x++)
+                {
+                    if (map[x, y] > max)
+                    {
+                        max = map[x, y];
+                    }
+                    if (map[x, y] < min)
+                    {
+                        min = map[x, y];
+                    }
+                }
+            }
+            
+            return (min, max);
+        }
+    }
+}
--- Assets/Scripts/LinearConverter.cs
+++ Assets/Scripts/LinearConverter.cs
@@ -36,6 +36,52 @@ namespace OctanGames
             return value * 2 - 1;
         }
 
+        /// <summary>
+        /// Convert all values of map to color
+        /// </summary>
+        /// <param name="map"></param>
+        public static void ConvertToColor(this float[,] map)
+        {
+            int width = map.GetLength(0);
+            int height = map.GetLength(1);
+
+            for (var y = 0; y < height; y++)
+            {
+                for (var x = 0; x < width; x++)
+                {
+                    map[x, y] = CoordinateToColor(map[x, y]);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Copy map and convert all values of map to color
+        /// </summary>
+        /// <param name="map"></param>
+        public static float[,] GetConvertedToColor(this float[,] map)
+        {
+            int width = map.GetLength(0);
+            int height = map.GetLength(1);
+
+            var newMap = new float[width, height];
+
+            for (var y = 0; y < height; y++)
+            {
+                for (var x = 0; x < width; x++)
+                {
+                    newMap[x, y] = CoordinateToColor(map[x, y]);
+                }
+            }
+
+            return newMap;
+        }
+
+        /// <summary>
+        /// Normalize all values between min and max
+        /// </summary>
+        /// <param name="map"></param>
+        /// <param name="minValue"></param>
+        /// <param name="maxValue"></param>
         public static void Normalize(this float[,] map, float minValue, float maxValue)
         {
             int width = map.GetLength(0);
@@ -50,7 +96,14 @@ namespace OctanGames
             }
         }
 
-        public static float[,] Normalized(this float[,] map, float minValue, float maxValue)
+        /// <summary>
+        /// Copy map and normalize all values between min and max
+        /// </summary>
+        /// <param name="map"></param>
+        /// <param name="minValue"></param>
+        /// <param name="maxValue"></param>
+        /// <returns></returns>
+        public static float[,] GetNormalized(this float[,] map, float minValue, float maxValue)
         {
             int width = map.GetLength(0);
             int height = map.GetLength(1);
