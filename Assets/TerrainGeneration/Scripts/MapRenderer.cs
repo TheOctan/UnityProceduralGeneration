1234118f04a9ac81ecf9b3da6a481e2b0c01ab3a
TheOctan <kanatush.tim@gmail.com>
2022-07-08 17:59:36 +0300

    Upgrade render mode swithing

--- Assets/TerrainGeneration/Scripts/MapRenderer.cs
+++ Assets/TerrainGeneration/Scripts/MapRenderer.cs
@@ -1,22 +1,36 @@
+using System;
 using OctanGames.TerrainGeneration.Scripts.Data;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace OctanGames.TerrainGeneration.Scripts
 {
     public class MapRenderer : MonoBehaviour
     {
-        [SerializeField] private Renderer _renderer;
+        [SerializeField] private Renderer _textureRenderer;
         [SerializeField] private MeshFilter _meshFilter;
         [SerializeField] private MeshRenderer _meshRenderer;
 
+        private void Start()
+        {
+            _textureRenderer.gameObject.SetActive(false);
+            _meshRenderer.gameObject.SetActive(false);
+        }
+
         public void DrawTexture(Texture2D texture)
         {
-            _renderer.sharedMaterial.mainTexture = texture;
-            _renderer.transform.localScale = new Vector3(texture.width, 1, texture.height);
+            _textureRenderer.gameObject.SetActive(true);
+            _meshRenderer.gameObject.SetActive(false);
+
+            _textureRenderer.sharedMaterial.mainTexture = texture;
+            _textureRenderer.transform.localScale = new Vector3(texture.width, 1, texture.height);
         }
 
         public void DrawMesh(MeshData mesh, Texture2D texture)
         {
+            _textureRenderer.gameObject.SetActive(false);
+            _meshRenderer.gameObject.SetActive(true);
+
             _meshFilter.sharedMesh = mesh.CreateMesh();
             _meshRenderer.sharedMaterial.mainTexture = texture;
         }
